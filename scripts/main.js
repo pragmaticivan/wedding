8edead4ac958089d0d93e79a95f30b375e0a580c
Ivan Santos <pragmaticivan@gmail.com>
2017-08-30 19:08:59 -0700

    Update 2017-08-31T02:08:56.670Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";document.addEventListener("DOMContentLoaded",function(e){var t=document.querySelector(".grid");new Masonry(t,{itemSelector:".grid-item",transitionDuration:0})});
+"use strict";document.addEventListener("DOMContentLoaded",function(e){var t=document.querySelector(".grid");imagesLoaded(t,function(){new Masonry(t,{itemSelector:".grid-item",transitionDuration:0})})});
